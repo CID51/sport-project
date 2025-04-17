9783ff0952803ca90ad218d5e8cd094b9e5e2269
Cheng In Du <cdu@bayviewglen.ca>
2025-04-17 19:03:41 -0400

    screw local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', 
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [vue()]
 })
